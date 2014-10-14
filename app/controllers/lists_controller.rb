94c90f054223dfe42283499a85286865daff5de5
John Allen <john_allen3@student.uml.edu>
2014-10-13 23:22:10 -0400

    Added some user auth, categories broken

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,10 +9,14 @@ class ListsController < ApplicationController
 
   def new
     @list = List.new
+    @user = current_user
   end
 
   def create
     @list = List.new(list_params)
+    @list.user_id = current_user.id
+    binding.pry
+    @list.categories = params[:list][:category_ids]
     if @list.save
       flash[:notice] = "List Created"
       redirect_to list_path(@list)
@@ -24,9 +28,11 @@ class ListsController < ApplicationController
 
   def edit
     @list = List.find(params[:id])
+    authenticate_user_for_action!
   end
 
   def update
+    authenticate_user_for_action!
     @list = List.find(params[:id])
     if @list.update(list_params)
       flash[:notice] = "List updated"
@@ -40,12 +46,19 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:title, :content)
+    params.require(:list).permit(:title, :content, :category_ids)
   end
 
   def authenticate_user_for_action!
     unless current_user == @list.user
       flash[:notice] = "You aren't authorized to do that."
+      redirect_to 'root_path'
+    end
+  end
+
+  def signed_in?
+    unless current_user
+      flash[:notice] = "You must sign in first!"
       redirect_to 'root_path'
     end
   end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,8 @@
 class List < ActiveRecord::Base
   has_many :categorizations
   has_many :categories, through: :categorizations
-
+  belongs_to :user
+  
   validates :title, presence: true
   validates :content, presence: true
 end
