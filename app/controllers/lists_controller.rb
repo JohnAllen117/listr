5de3ca6c3834e5f0b2544819b92f067ccb249bb6
John Allen <john_allen3@student.uml.edu>
2014-10-14 10:52:29 -0400

    cleaned up categories RFP

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+  before_action :signed_in?, only: [:new, :create, :edit, :update, :destroy]
   def index
     @lists = List.all
   end
@@ -17,10 +18,10 @@ class ListsController < ApplicationController
     @list.user_id = current_user.id
 
     if @list.save
-      flash[:notice] = "List Created"
+      flash[:notice] = 'List Created'
       redirect_to list_path(@list)
     else
-      flash[:notice] = "Invalid Entry"
+      flash[:notice] = 'Invalid Entry'
       render :new
     end
   end
@@ -34,14 +35,22 @@ class ListsController < ApplicationController
     authenticate_user_for_action!
     @list = List.find(params[:id])
     if @list.update(list_params)
-      flash[:notice] = "List updated"
+      flash[:notice] = 'List updated'
       redirect_to list_path(@list)
     else
-      flash[:notice] = "Invalid Entry"
+      flash[:notice] = 'Invalid Entry'
       render :edit
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    authenticate_user_for_action!
+
+    List.destroy(params[:id])
+
+    redirect_to root_path, notice: 'List deleted.'
+  end
   private
 
   def list_params
@@ -51,14 +60,14 @@ class ListsController < ApplicationController
   def authenticate_user_for_action!
     unless current_user == @list.user
       flash[:notice] = "You aren't authorized to do that."
-      redirect_to 'root_path'
+      redirect_to root_path
     end
   end
 
   def signed_in?
     unless current_user
-      flash[:notice] = "You must sign in first!"
-      redirect_to 'root_path'
+      flash[:notice] = 'You must sign in first!'
+      redirect_to root_path
     end
   end
 end
