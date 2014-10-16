c6b131c3611c87023445e40352ac9337927439d1
John Allen <john_allen3@student.uml.edu>
2014-10-16 11:32:05 -0400

    hound formatting

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe List do
-  it { should respond_to(:user)}
-  it { should respond_to(:categories)}
-  it { should respond_to(:likes)}
+  it { should respond_to(:user) }
+  it { should respond_to(:categories) }
+  it { should respond_to(:likes) }
 end
