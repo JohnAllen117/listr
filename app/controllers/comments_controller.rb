6952f425833c3d12e900a4c1e7bd7836a58da7e9
John Allen <john_allen3@student.uml.edu>
2014-10-22 18:46:09 -0400

    new comment model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,20 @@
 class CommentsController < ApplicationController
   before_action :signed_in?, only: [:new, :create, :edit, :update, :destroy]
   def new
-
+    @comment = Comment.new
+    @user = current_user
   end
 
   def create
-
+    @comment = Comment.new(comment_params)
+    @list = @comment.list 
+    if @comment.save
+      flash[:notice] = "Comment Created"
+      redirect_to list_path(@list)
+    else
+      flash[:notice] = "Invalid Entry"
+      render :new
+    end
   end
 
   def edit
