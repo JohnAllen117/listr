589fb7319ddc09ce885c24535c2c546a8359e26d
John Allen <john_allen3@student.uml.edu>
2014-10-23 21:53:19 -0400

    added comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,5 @@ class Comment < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :list_id, presence: true
+  validates_length_of :content, in: 1..10000
 end
--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -9,7 +9,16 @@ feature "User comments on a list" do
     click_on "New Comment"
     fill_in "Content", with: "This is a comment"
     click_on "Create Comment"
-    
     expect(page).to have_content("Comment Created")
+    expect(page).to have_content("This is a comment")
   end
+  # 
+  # scenario "unsuccessfully" do
+  #   sign_in_as(user)
+  #   visit list_path(list)
+  #   click_on "New Comment"
+  #   fill_in "Content", with: nil
+  #   click_on "Create Comment"
+  #   expect(page).to have_content "Invalid Entry"
+  # end
 end
